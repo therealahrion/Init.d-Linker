04cf67fb9c0fc4d406311185087ae30359d8f504
Zackptg5 <zackptg5@comcast.net>
2017-10-23 17:41:58 -0400

    add modid

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -2,7 +2,7 @@
 # Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
-
+MODID=0000initdlinker
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 
--- common/service.sh
+++ common/service.sh
@@ -2,7 +2,7 @@
 # Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
-
+MODID=0000initdlinker
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 
