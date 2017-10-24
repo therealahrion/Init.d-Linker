f54778931396aa9dd5f30ac0c8ca38994bb0dffa
therealahrion <ahrionmgallegos@gmail.com>
2017-10-24 08:20:56 -0600

    merge test branch and other fixes/.core move

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,34 +1,11 @@
-UNINSTALL=false
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
 
-if [ ! -d /magisk/$MODID ]; then
-  rm -f /magisk/.core/post-fs-data.d/$MODID.sh
-  UNINSTALL=true
-  rm -rf /magisk/.core/post-fs-data.d/init.d
-fi
+# This script will be executed in post-fs-data mode
+# More info in the main Magisk thread
 
-if [ $UNINSTALL == false ]; then
-  if [ ! -d $SYS/etc/init.d ]; then
-    mkdir $SYS/etc/init.d
-    chmod 755 $SYS/etc/init.d
-  fi
+test ! -d /magisk/0000initdlinker && rm -f /magisk/.core/post-fs-data.d/0000initdlinker.sh && exit
 
-  if [ -f /magisk/$MODID/post-fs-data.sh ]; then
-    mkdir /magisk/.core/post-fs-data.d/init.d
-    chmod 755 /magisk/.core/post-fs-data.d/init.d
-    mv -f /magisk/$MODID/post-fs-data.sh /magisk/.core/post-fs-data.d/$MODID.sh
-    chmod 755 /magisk/.core/post-fs-data.d/$MODID.sh
-  fi
-
-  if [ -f $SYS/etc/init.d/0000liveboot ]; then
-    ln -sfn $SYS/etc/init.d/0000liveboot /magisk/.core/post-fs-data.d/init.d/0000liveboot
-  fi
-
-  mkdir /magisk/.core/.hidden
-  mv -f /magisk/.core/post-fs-data.d/$MODID.sh /magisk/.core/.hidden/$MODID.sh
-
-  su -c sh /magisk/.core/post-fs-data.d/*
-  su -c sh /magisk/.core/post-fs-data.d/init.d/*
-
-  mv -f /magisk/.core/.hidden/$MODID.sh /magisk/.core/post-fs-data.d/$MODID.sh
-  rm -f /magisk/.core/.hidden
-fi
+test -f /system/etc/init.d/0000liveboot && su -c sh /system/etc/init.d/0000liveboot
--- common/service.sh
+++ common/service.sh
@@ -1,37 +1,13 @@
-UNINSTALL=false
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
 
-if [ ! -d /magisk/$MODID ]; then
-  rm -f /magisk/.core/service.d/$MODID.sh
-  UNINSTALL=true
-  rm -rf /magisk/.core/service.d/init.d
-  reboot
-fi
+# This script will be executed in late_start service mode
+# More info in the main Magisk thread
 
-if [ $UNINSTALL == false ]; then
-  if [ -f /magisk/$MODID/service.sh ]; then
-    mkdir /magisk/.core/service.d/init.d
-    chmod 755 /magisk/.core/service.d/init.d
-    mv -f /magisk/$MODID/service.sh /magisk/.core/service.d/$MODID.sh
-    chmod 755 /magisk/.core/service.d/$MODID.sh
-    FIRSTBOOT=true
-  fi
+test ! -d /magisk/0000initdlinker && rm -f /magisk/.core/service.d/0000initdlinker.sh && exit
 
-  ln -sfn $SYS/etc/init.d/* /magisk/.core/service.d/init.d
-
-  if [ -f $SYS/etc/init.d/0000liveboot ]; then
-    rm -f /magisk/.core/service.d/init.d/0000liveboot
-  fi
-
-  mkdir /magisk/.core/.hidden
-  mv -f /magisk/.core/service.d/$MODID.sh /magisk/.core/.hidden/$MODID.sh
-
-  su -c sh /magisk/.core/service.d/*
-  su -c sh /magisk/.core/service.d/init.d/*
-
-  mv -f /magisk/.core/.hidden/$MODID.sh /magisk/.core/service.d/$MODID.sh
-  rm -r /magisk/.core/.hidden
-
-  if [ $FIRSTBOOT == true ]; then
-    reboot
-  fi
-fi
+for FILE in /system/etc/init.d/*; do
+  test "$FILE" != "/system/etc/init.d/0000liveboot" && su -c sh $FILE &
+done
